07de1b0177acaa9070698ba7c36209b708dcd264
pilevsky <pilevskiyd@yandex.ru>
2023-06-22 20:38:47 +0600

    Lesson4

---
+++ main.js
@@ -0,0 +1,11 @@
+// let a = prompt('Введите число а');
+// let b = prompt('Введите число b');
+// let c = (parseInt(a) + parseInt(b)) <= 15 ? console.log('Нет') : console.log('Да')
+
+
+
+let a = prompt('Введите переменную а');
+let b = prompt('Введите переменную b');
+let c = a === b ? console.log('Обе перменные равны ' + a) : console.log('Разные переменные')
+
+
